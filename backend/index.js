d88e6b92ea812e6e9dddd26023f48fe0f5ccc761
jyotikumari1510 <jyoti.kumari@pravisht.com>
2025-08-25 15:18:03 +0530

    category routes

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import userRoutes from "./router/userRoutes.js";
+import catergoryRoutes from "./router/categoryRoutes.js";
 import cookieParser from "cookie-parser";
-
 
 const app = express();
 dotenv.config();
@@ -15,13 +15,13 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 };
 
-
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/user',userRoutes);
+app.use("/user", userRoutes);
+app.use("/category", catergoryRoutes);
 
 const port = process.env.port;
 
